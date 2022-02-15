a642e32013941a79f5a77e1e1e4d48b99e30f436
TajalTechnology <mrpresident1189@gmail.com>
2022-02-16 01:24:40 +0600

    working with app seetings

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,1 +1,18 @@
-console.log('Hello is from tajal');
+import utils from '../common/services/utils';
+
+/* ----------------------------------- */
+/* Initializing Express App */
+/* ----------------------------------- */
+var appLocals = {
+    'baseUri': '/api/',
+    'dirname': __dirname,
+};
+
+var app = utils.initApp(appLocals);
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization');
+    next();
+});

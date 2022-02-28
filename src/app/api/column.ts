5e77afe77e3e8fa0ef77884bfd394448e2a7a396
Greeho951 <greeho951@gmail.com>
2022-02-28 16:51:22 +0600

    columns documentation done

--- src/app/api/column.ts
+++ src/app/api/column.ts
@@ -15,7 +15,7 @@ export const tryCatch = (fn: any) => (req: Request, res: Response, next: NextFun
 module.exports = function (router: Express) {
 
     router.get('/columns', tryCatch(getAllColumnsHandler));
-    router.post('/columns', authCheck, validation(createColumnSchema), tryCatch(createColumnHandler));
+    router.post('/columns', validation(createColumnSchema), tryCatch(createColumnHandler));
     router.get('/columns/:columnsId', validation(getColumnsSchema), tryCatch(getColumnHandler));
     router.put('/columns/:columnsId', validation(updateColumnSchema), tryCatch(updateColumnsHandler));
     router.delete('/columns/:columnsId', validation(deleteColumnSchema), tryCatch(deleteColumnsHandler));
@@ -42,7 +42,7 @@ async function createColumnHandler(_req: Request, _res: Record<string, any>) {
 /* handler for get extra columns of a single form */
 export async function getColumnHandler(_req: Request<getColumnsInput["params"]>, _res: Record<string, any>) {
     const extraColumnsDetails = await getColumn({ _id: _req.params.columnsId });
-    if (extraColumnsDetails._id.toString() !== _req.params.columnsId) return _res.apiDataNotFound(extraColumnsDetails);
+    if (extraColumnsDetails._id !== _req.params.columnsId) return _res.apiDataNotFound(extraColumnsDetails);
     return _res.apiSuccess(extraColumnsDetails);
 };
 

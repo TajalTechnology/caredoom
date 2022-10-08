12c9e47d3080eb6e5ca99423c313cb140919129a
TajalTechnology <mrpresident1189@gmail.com>
2022-10-08 14:56:12 +0600

    ADD: lid auth apis

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -1,10 +1,7 @@
-var router = require('express').Router();
-const fs = require('fs');
+var router = require("express").Router();
+const fs = require("fs");
 
-require('./form')(router);
-require('./column')(router);
-require('./test')(router);
-require('./user')(router);
-require('./session')(router);
+require("./user")(router);
+require("./session")(router);
 
 module.exports = router;
--- src/app/api/test.ts
+++
@@ -1,8 +0,0 @@
-module.exports = function (router: any) {
-    router.get('/test', add);
-};
-
-function add(_req: any, _res: any) {
-    const respons = 'Hello';
-    return _res.apiSuccess(respons);
-}

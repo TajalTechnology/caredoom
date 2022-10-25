894f621903e980c9d14d94b04e86f472581f56e1
TajalTechnology <mrpresident1189@gmail.com>
2022-10-25 17:21:26 +0600

    READY: deploy v1.0

--- server.ts
+++ server.ts
@@ -3,6 +3,5 @@ import dotenv_safe from "dotenv-safe";
 dotenv_safe.config({ allowEmptyValues: true });
 
 /* app running configuration */
-import path from "path";
 process.env.BASE_PATH = process.cwd();
 import(process.env.BASE_PATH + "/src/app/app.ts");

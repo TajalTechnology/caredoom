341e8befc70af280557800e8c594b252e38d73e1
TajalTechnology <mrpresident1189@gmail.com>
2022-11-19 18:35:15 +0600

    UPDATE

--- server.ts
+++ server.ts
@@ -3,5 +3,4 @@ import dotenv_safe from "dotenv-safe";
 dotenv_safe.config({ allowEmptyValues: false });
 
 /* app running configuration */
-process.env.BASE_PATH = process.cwd();
-import(process.env.BASE_PATH + "/src/app/app.ts");
+require("./src/app/app");

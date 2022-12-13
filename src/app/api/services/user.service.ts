fa6294b7a327e0735f3acbd32ac21af1c6718102
TajalTechnology <mrpresident1189@gmail.com>
2022-12-13 21:24:30 +0600

    DELETE api

--- src/app/api/services/user.service.ts
+++ src/app/api/services/user.service.ts
@@ -8,172 +8,184 @@ import config from "config";
 import bcrypt from "bcrypt";
 
 export async function createUser(input: Record<string, any>) {
-  const responseData: any = {};
-  if (input.phnNo) input.phnNo = "+880" + input.phnNo;
-  //TODO: need to optimize this query
-  let duplicateUser: any;
-  if (input.email && !input.phnNo) {
-    duplicateUser = await UserModel.findOne({ email: input.email });
-  } else if (!input.email && input.phnNo) {
-    duplicateUser = await UserModel.findOne({ phnNo: input.phnNo });
-  } else {
-    duplicateUser = await UserModel.findOne({
-      $or: [{ email: input.email }, { phnNo: input.phnNo }],
-    });
-  }
+    const responseData: any = {};
+    if (input.phnNo) input.phnNo = "+880" + input.phnNo;
+    //TODO: need to optimize this query
+    let duplicateUser: any;
+    if (input.email && !input.phnNo) {
+        duplicateUser = await UserModel.findOne({ email: input.email });
+    } else if (!input.email && input.phnNo) {
+        duplicateUser = await UserModel.findOne({ phnNo: input.phnNo });
+    } else {
+        duplicateUser = await UserModel.findOne({
+            $or: [{ email: input.email }, { phnNo: input.phnNo }],
+        });
+    }
 
-  !duplicateUser
-    ? (responseData.data = await UserModel.create(input))
-    : (responseData.message = _responce.duplicateUser);
+    !duplicateUser
+        ? (responseData.data = await UserModel.create(input))
+        : (responseData.message = _responce.duplicateUser);
 
-  return responseData;
+    return responseData;
 }
 
 export async function verifyOTP(
-  input: UpdateQuery<UserDocument>,
-  options: QueryOptions
+    input: UpdateQuery<UserDocument>,
+    options: QueryOptions
 ) {
-  if (input.phnNo) input.phnNo = "+880" + input.phnNo;
+    if (input.phnNo) input.phnNo = "+880" + input.phnNo;
 
-  const responseData: any = {};
-  const verifyWith = input.email ? input.email : input.phnNo;
-  console.log("Input:", input);
+    const responseData: any = {};
+    const verifyWith = input.email ? input.email : input.phnNo;
+    console.log("Input:", input);
 
-  const filter = {
-    $and: [
-      { $or: [{ email: verifyWith }, { phnNo: verifyWith }] },
-      { verificationCode: input.verificationCode },
-      // {
-      //   $or: [{ verificationCode: input.verificationCode }, { isVerify: true }],
-      // },
-    ],
-  };
-  const update = { verificationCode: null, isVerify: true };
-  const user: any = await UserModel.findOneAndUpdate(filter, update, options);
+    const filter = {
+        $and: [
+            { $or: [{ email: verifyWith }, { phnNo: verifyWith }] },
+            { verificationCode: input.verificationCode },
+            // {
+            //   $or: [{ verificationCode: input.verificationCode }, { isVerify: true }],
+            // },
+        ],
+    };
+    const update = { verificationCode: null, isVerify: true };
+    const user: any = await UserModel.findOneAndUpdate(filter, update, options);
 
-  if (user?.isVerify === true) {
-    responseData.sucess = _responce.verificationSucess;
-  } else {
-    responseData.failed = _responce.verificationFailed;
-  }
-  return responseData;
+    if (user?.isVerify === true) {
+        responseData.sucess = _responce.verificationSucess;
+    } else {
+        responseData.failed = _responce.verificationFailed;
+    }
+    return responseData;
 }
 
 export async function createLogin(input: Record<string, any>) {
-  // TODO: sign in with otp
-  const responseData: any = {};
-  if (input.phnNo) input.phnNo = "+880" + input.phnNo;
-  let user: any;
-  if (input.verificationCode) {
-    user = await UserModel.findOneAndUpdate(
-      {
-        $and: [
-          { phnNo: input.phnNo },
-          { verificationCode: input.verificationCode },
-        ],
-      },
-      { verificationCode: null },
-      { new: true }
-    );
-  } else {
-    const loginWith = input.email
-      ? input.email
-      : input.phnNo
-      ? input.phnNo
-      : input.username;
+    // TODO: sign in with otp
+    const responseData: any = {};
+    if (input.phnNo) input.phnNo = "+880" + input.phnNo;
+    let user: any;
+    if (input.verificationCode) {
+        user = await UserModel.findOneAndUpdate(
+            {
+                $and: [
+                    { phnNo: input.phnNo },
+                    { verificationCode: input.verificationCode },
+                ],
+            },
+            { verificationCode: null },
+            { new: true }
+        );
+    } else {
+        const loginWith = input.email
+            ? input.email
+            : input.phnNo
+            ? input.phnNo
+            : input.username;
 
-    user = await UserModel.findOne({
-      $or: [
-        { email: loginWith },
-        { phnNo: loginWith },
-        { username: loginWith },
-      ],
-    });
+        user = await UserModel.findOne({
+            $or: [
+                { email: loginWith },
+                { phnNo: loginWith },
+                { username: loginWith },
+            ],
+        });
 
-    //TODO: shoud be implementing as a differenct function
-    const valid = await bcrypt
-      .compare(input.password, user.password)
-      .catch((e) => false);
-    if (!valid) {
-      return (responseData.message = "Incorrect account or password");
+        //TODO: shoud be implementing as a differenct function
+        const valid = await bcrypt
+            .compare(input.password, user.password)
+            .catch((e) => false);
+        if (!valid) {
+            return (responseData.message = "Incorrect account or password");
+        }
     }
-  }
 
-  if (!user) {
-    return (responseData.message = "Wrong credentials.Please try again");
-  }
+    if (!user) {
+        return (responseData.message = "Wrong credentials.Please try again");
+    }
 
-  const login = await LoginModel.create({ user: user._id });
+    const login = await LoginModel.create({ user: user._id });
 
-  // create an access token
-  const accessToken = signJwt(
-    { ...user, session: login._id },
-    { expiresIn: config.get("accessTokenTtl") } // 15 minutes
-  );
+    // create an access token
+    const accessToken = signJwt(
+        { ...user, session: login._id },
+        { expiresIn: config.get("accessTokenTtl") } // 15 minutes
+    );
 
-  // create a refresh token
-  const refreshToken = signJwt(
-    { ...user, session: login._id },
-    { expiresIn: config.get("refreshTokenTtl") } // 15 minutes
-  );
+    // create a refresh token
+    const refreshToken = signJwt(
+        { ...user, session: login._id },
+        { expiresIn: config.get("refreshTokenTtl") } // 15 minutes
+    );
 
-  if (accessToken && refreshToken) {
-    return (responseData.data = { accessToken, refreshToken });
-  }
-  // }
+    if (accessToken && refreshToken) {
+        return (responseData.data = { accessToken, refreshToken });
+    }
+    // }
 
-  // return (responseData.message = "Incorrect account or password");
+    // return (responseData.message = "Incorrect account or password");
 }
 
 export async function resendOtp(
-  input: UpdateQuery<UserDocument>,
-  options: QueryOptions
+    input: UpdateQuery<UserDocument>,
+    options: QueryOptions
 ) {
-  if (input.phnNo) input.phnNo = "+880" + input.phnNo;
-  const responseData: any = {};
-  let otp = "123456";
+    if (input.phnNo) input.phnNo = "+880" + input.phnNo;
+    const responseData: any = {};
+    let otp = "123456";
 
-  const otpSend = input.email ? input.email : input.phnNo;
-  const filter = {
-    $or: [{ email: otpSend }, { phnNo: otpSend }],
-  };
-  const update = { verificationCode: otp };
-  const user = await UserModel.findOneAndUpdate(filter, update, options);
+    const otpSend = input.email ? input.email : input.phnNo;
+    const filter = {
+        $or: [{ email: otpSend }, { phnNo: otpSend }],
+    };
+    const update = { verificationCode: otp };
+    const user = await UserModel.findOneAndUpdate(filter, update, options);
 
-  // TODO: need to send code in mail
-  user
-    ? (responseData.verificationCode = user.verificationCode)
-    : (responseData.message = "Your credential may be wrong. Try again");
-  return responseData;
+    // TODO: need to send code in mail
+    user
+        ? (responseData.verificationCode = user.verificationCode)
+        : (responseData.message = "Your credential may be wrong. Try again");
+    return responseData;
 }
 
 export async function resetPassword(
-  input: UpdateQuery<UserDocument>,
-  options: QueryOptions
+    input: UpdateQuery<UserDocument>,
+    options: QueryOptions
 ) {
-  const responseData: any = {};
-  if (input.phnNo) input.phnNo = "+880" + input.phnNo;
-  const hash = bcrypt.hashSync(
-    input.password,
-    await bcrypt.genSalt(config.get<number>("saltWorkFactor"))
-  );
+    const responseData: any = {};
+    if (input.phnNo) input.phnNo = "+880" + input.phnNo;
+    const hash = bcrypt.hashSync(
+        input.password,
+        await bcrypt.genSalt(config.get<number>("saltWorkFactor"))
+    );
 
-  const resetPassWith = input.email ? input.email : input.phnNo;
-  const filter = {
-    $and: [
-      { $or: [{ email: resetPassWith }, { phnNo: resetPassWith }] },
-      { isVerify: true },
-    ],
-  };
-  const update = {
-    verificationCode: null,
-    password: hash,
-  };
-  const user = await UserModel.findOneAndUpdate(filter, update, options);
+    const resetPassWith = input.email ? input.email : input.phnNo;
+    const filter = {
+        $and: [
+            { $or: [{ email: resetPassWith }, { phnNo: resetPassWith }] },
+            { isVerify: true },
+        ],
+    };
+    const update = {
+        verificationCode: null,
+        password: hash,
+    };
+    const user = await UserModel.findOneAndUpdate(filter, update, options);
 
-  user
-    ? (responseData.data = true)
-    : (responseData.message = _responce.passwordUpdateFailed);
+    user
+        ? (responseData.data = true)
+        : (responseData.message = _responce.passwordUpdateFailed);
 
-  return responseData;
+    return responseData;
+}
+
+export async function deleteUser(input: UpdateQuery<UserDocument>) {
+    const responseData: any = {};
+    if (input.phnNo) input.phnNo = "+880" + input.phnNo;
+    const user = await UserModel.findOneAndDelete({ email: input.email });
+
+    user
+        ? (responseData.data = true)
+        : (responseData.message = _responce.passwordUpdateFailed);
+
+    return responseData;
 }
--- src/app/api/user.ts
+++ src/app/api/user.ts
@@ -1,128 +1,129 @@
 import validation from "../common/middlewares/validation";
 import { NextFunction, Request, Response, Express } from "express";
 import {
-  createUserInput,
-  createUserSchema,
-  verifyOtpSchema,
-  verifyOTPInput,
-  createLoginSchema,
-  createLoginInput,
-  resendOtpSchema,
-  resendOtpInput,
-  resetPasswordSchema,
-  resetPasswordInput,
+    createUserInput,
+    createUserSchema,
+    verifyOtpSchema,
+    verifyOTPInput,
+    createLoginSchema,
+    createLoginInput,
+    resendOtpSchema,
+    resendOtpInput,
+    resetPasswordSchema,
+    resetPasswordInput,
 } from "../schemas/user.schema";
 import {
-  createLogin,
-  createUser,
-  resendOtp,
-  resetPassword,
-  verifyOTP,
+    createLogin,
+    createUser,
+    deleteUser,
+    resendOtp,
+    resetPassword,
+    verifyOTP,
 } from "./services/user.service";
 
 /* try-catch handle */
 const tryCatch =
-  (fn: any) => (req: Request, res: Response, next: NextFunction) =>
-    Promise.resolve(fn(req, res, next)).catch(next);
+    (fn: any) => (req: Request, res: Response, next: NextFunction) =>
+        Promise.resolve(fn(req, res, next)).catch(next);
 
 /* all routes */
 module.exports = function (router: Express) {
-  router.post(
-    "/users/registration",
-    validation(createUserSchema),
-    tryCatch(createUserHandler)
-  );
+    router.post(
+        "/users/registration",
+        validation(createUserSchema),
+        tryCatch(createUserHandler)
+    );
 
-  router.put(
-    "/users/password_set",
-    validation(resetPasswordSchema),
-    tryCatch(setPasswordHandler)
-  );
+    router.put(
+        "/users/password_set",
+        validation(resetPasswordSchema),
+        tryCatch(setPasswordHandler)
+    );
 
-  router.put(
-    "/users/otp_verify",
-    validation(verifyOtpSchema),
-    tryCatch(verifyOtpHandler)
-  );
+    router.put(
+        "/users/otp_verify",
+        validation(verifyOtpSchema),
+        tryCatch(verifyOtpHandler)
+    );
 
-  router.post(
-    "/users/login",
-    validation(createLoginSchema),
-    tryCatch(loginHandler)
-  );
+    router.post(
+        "/users/login",
+        validation(createLoginSchema),
+        tryCatch(loginHandler)
+    );
 
-  router.put(
-    "/users/resend_otp",
-    validation(resendOtpSchema),
-    tryCatch(resendOtpHandler)
-  );
+    router.put(
+        "/users/resend_otp",
+        validation(resendOtpSchema),
+        tryCatch(resendOtpHandler)
+    );
+
+    router.delete("/users/delete", tryCatch(deleteUserHandler));
 };
 
 async function createUserHandler(
-  _req: Request<{}, {}, createUserInput["body"]>,
-  _res: Record<string, any>
+    _req: Request<{}, {}, createUserInput["body"]>,
+    _res: Record<string, any>
 ) {
-  const response = await createUser(_req.body);
+    const response = await createUser(_req.body);
 
-  if (response.data) return _res.apiSuccess(response);
-  else return _res.apiDuplicate(response);
+    if (response.data) return _res.apiSuccess(response);
+    else return _res.apiDuplicate(response);
 }
 
 async function verifyOtpHandler(
-  _req: Request<{}, {}, verifyOTPInput["body"]>,
-  _res: Record<string, any>
+    _req: Request<{}, {}, verifyOTPInput["body"]>,
+    _res: Record<string, any>
 ) {
-  const response = await verifyOTP(_req.body as any, {
-    new: true,
-  });
-  console.log(response);
+    const response = await verifyOTP(_req.body as any, {
+        new: true,
+    });
+    console.log(response);
 
-  if (response.sucess) {
-    return _res.apiSuccess(response);
-  } else {
-    return _res.apiDataNotFound(response);
-  }
+    if (response.sucess) {
+        return _res.apiSuccess(response);
+    } else {
+        return _res.apiDataNotFound(response);
+    }
 }
 
 async function loginHandler(
-  _req: Request<{}, {}, createLoginInput["body"]>,
-  _res: Record<string, any>
+    _req: Request<{}, {}, createLoginInput["body"]>,
+    _res: Record<string, any>
 ) {
-  const response: any = await createLogin(_req.body);
+    const response: any = await createLogin(_req.body);
 
-  if (response.accessToken && response.refreshToken)
-    return _res.apiSuccess(response);
-  else return _res.apiDuplicate(response);
+    if (response.accessToken && response.refreshToken)
+        return _res.apiSuccess(response);
+    else return _res.apiDuplicate(response);
 }
 
 async function resendOtpHandler(
-  _req: Request<{}, {}, resendOtpInput["body"]>,
-  _res: Record<string, any>
+    _req: Request<{}, {}, resendOtpInput["body"]>,
+    _res: Record<string, any>
 ) {
-  const response: any = await resendOtp(_req.body as any, { new: true });
+    const response: any = await resendOtp(_req.body as any, { new: true });
 
-  if (response.verificationCode) return _res.apiSuccess(response);
-  else return _res.apiDataNotFound(response);
+    if (response.verificationCode) return _res.apiSuccess(response);
+    else return _res.apiDataNotFound(response);
 }
 
 async function setPasswordHandler(
-  _req: Request<{}, {}, resetPasswordInput["body"]>,
-  _res: Record<string, any>
+    _req: Request<{}, {}, resendOtpInput["body"]>,
+    _res: Record<string, any>
 ) {
-  const response: any = await resetPassword(_req.body as any, {
-    new: true,
-  });
-  if (response.data) return _res.apiSuccess(response);
-  else return _res.apiDataNotFound(response);
-  // const otpVerify: any = await verifyOTP(_req.body as any, {
-  //   new: true,
-  // });
+    const response: any = await resetPassword(_req.body as any, {
+        new: true,
+    });
+    if (response.data) return _res.apiSuccess(response);
+    else return _res.apiDataNotFound(response);
+}
 
-  // if (otpVerify) {
-  //   const response: any = await resetPassword(_req.body as any, {
-  //     new: true,
-  //   });
-  //   if (response.data) return _res.apiSuccess(response);
-  //   else return _res.apiDataNotFound(otpVerify);
-  // } else return _res.apiDataNotFound(otpVerify);
+async function deleteUserHandler(
+    _req: Request<{}, {}, resendOtpInput["body"]>,
+    _res: Record<string, any>
+) {
+    const response: any = await deleteUser(_req.body as any);
+    if (response.data) return _res.apiSuccess(response);
+    else return _res.apiDataNotFound(response);
 }
